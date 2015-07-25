8d6459055e44e309dda33157d7c7b57184f29794
Huy Do <huy.do@gmx.de>
2015-07-25 16:58:35 +0200

    initial commit

---
+++ clib/clock.h
@@ -0,0 +1,16 @@
+#ifndef _CLOCK_H_
+#define _CLOCK_H_
+
+volatile extern uint32_t ticks;  // 1/125 sec resolution
+void gettime(char*);
+
+#ifdef HAS_ETHERNET
+
+typedef uint16_t clock_time_t;
+clock_time_t clock_time(void);
+
+#endif
+
+void Minute_Task(void);
+
+#endif
---
+++ version.h
@@ -0,0 +1,3 @@
+#define VERSION_1               1
+#define VERSION_2               61
+#define VERSION                 "1.61"
